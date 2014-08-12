b9539c3549993499247b4a425052b00e54c01f79
inferrna <inferrna@gmail.com>
2014-08-12 17:52:15 +0900

    +queries

--- processes.py
+++ processes.py
@@ -1,12 +1,15 @@
 from multiprocessing import Process
 import os
 from subprocess import call
+from datetime import datetime
 
 script = "./runstation.sh"
 host  = "localhost"
 passw = "ad438a4635b7f6b7cd8b1185278ffbb2"
 live_streams = {}
-
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+c.execute('''CREATE TABLE clients(ip text, url text, ldate datetime)''')
 
 def info(title):
     print(title)
@@ -14,6 +17,16 @@ def info(title):
     if hasattr(os, 'getppid'):  # only available on Unix
         print('parent process:', os.getppid())
     print('process id:', os.getpid())
+
+def newclient(ip, surl)
+    c.execute('''INSERT INTO clients VALUES (?, ?, DateTime('now'));''', ip, surl)
+
+def updateclient(ip)
+    c.execute('''UPDATE clients SET ldate=? WHERE ip=?''', datatime.now().strftime('%Y-%m-%d %H%M%S'), ip1)
+
+def checkclient(ip)
+    c.execute('''select  from clients WHERE ldate <= DateTime('now', '-1200 seconds');''')
+
 
 def new_stream(surl, stype, name, client):
     #call(["./runstation.sh", "http://81.19.85.197/echo.mp3", "localhost", "mp3", "ad438a4635b7f6b7cd8b1185278ffbb2", "coge"])
