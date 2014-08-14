b7bc0a20b33ca0b50d1e852883e3d37b5d30dec1
inferrna <inferrna@gmail.com>
2014-08-14 18:44:02 +0900

    +simplegain

--- joinradios.js
+++ joinradios.js
@@ -12,19 +12,20 @@ function onclc(){
     source = ac.createMediaElementSource(audioElement);
     console.log('source');
     console.log(source);
-    source.connect(ac.destination);
-   // var splitter = ac.createChannelSplitter(2);
-   // source.connect(splitter);
-   // var merger = ac.createChannelMerger(2);
+    //source.connect(ac.destination);
+    //var splitter = ac.createChannelSplitter(2);
+    //source.connect(splitter);
+    var merger = ac.createChannelMerger(2);
 
-   // // Reduce the volume of the left channel only
-   // var gain = ac.createGain();
-   // gain.value = 0.5;
-   // splitter.connect(gain, 0);
+    // Reduce the volume of the left channel only
+    var gain = ac.createGain();
+    gain.value = 0.1;
+    //splitter.connect(gain, 0);
+    source.connect(gain, 0);
 
-   // gain.connect(merger, 0, 1);
-   // splitter.connect(merger, 1, 0);
+    gain.connect(merger, 0, 1);
+    source.connect(merger, 0, 0);
 
-   // console.log("Start playing");
-   // merger.connect(ac.destination);
+    console.log("Start playing");
+    merger.connect(ac.destination);
 }
